c3612e919c2da4948c1820fe81486ff74e2ac116
Ron Rademaker <ron@connectholland.nl>
2015-11-05 11:09:35 +0100

    Removed comments that should be in the builder

--- src/Aggregation/Unwind.php
+++ src/Aggregation/Unwind.php
@@ -20,9 +20,6 @@ class Unwind extends AbstractAggregation
     /**
      * Sets the field to unwind
      *
-     * The $allowMixedDatabases allows you to use this in databases holding both arrays and other values in the same field
-     * The use of this is discouraged both by Mongo and by me. Use this only if you must.
-     *
      * @param string $unwindField
      */
     public function setField($unwindField)
