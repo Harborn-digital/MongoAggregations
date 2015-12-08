8164a839bf7dc02247f8723d048e9560859d6fa0
Ron Rademaker <ron@connectholland.nl>
2015-12-08 12:58:42 +0100

    Added test for the sort stage

---
+++ tests/Aggregation/SortTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace ConnectHolland\MongoAggregations\Aggregation\Test;
+
+use ConnectHolland\MongoAggregations\Aggregation\Sort;
+use ConnectHolland\MongoAggregations\Test\AbstractTestCase;
+
+/**
+ * Unit test for the sort stage
+ *
+ * @author Ron Rademaker
+ */
+class SortTest extends AbstractTestCase
+{
+    /**
+     * Test sorting some data
+     */
+    public function testSortData()
+    {
+        $testData = [
+            ['foo' => 1],
+            ['foo' => 54],
+            ['foo' => 2],
+            ['foo' => 5],
+            ['foo' => 435],
+            ['foo' => 17]
+        ];
+
+        foreach ($testData as $test) {
+            $this->collection->save($test);
+        }
+
+        $sort = new Sort();
+        $sort->addSort('foo', -1);
+
+        $result = $this->collection->aggregate([$sort->getStage()]);
+
+        $max = 500;
+        foreach ($result['result'] as $record) {
+           $this->assertLessThan($max, $record['foo']);
+           $max = $record['foo'];
+        }
+
+    }
+}
